596a5a9b12489c2636198f48f8f7918a6e631fbf
Vitor Martins <vitormartins.one@gmail.com>
2021-11-08 18:13:50 -0300

    listas

---
+++ list-sort-3-5-1-3.py
@@ -0,0 +1,12 @@
+my_list = [8, 10, 6, 2, 4]
+my_list.sort()
+print(my_list)
+
+# Há também um método de lista chamado reverse(), 
+# #que pode utilizar para inverter a lista, por exemplo
+
+lst = [5, 3, 1, 2, 4]
+print(lst)
+
+lst.reverse()
+print(lst)  # outputs: [4, 2, 1, 3, 5]
