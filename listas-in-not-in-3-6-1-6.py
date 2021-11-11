d240ec6df8601e25c6337e19707f14c80309ac32
Vitor Martins <vitormartins.one@gmail.com>
2021-11-11 18:54:42 -0300

    in not in

---
+++ listas-in-not-in-3-6-1-6.py
@@ -0,0 +1,13 @@
+# O Python oferece dois operadores muito poderosos, 
+# capazes de olhar através da lista a fim de verificar 
+# se um valor específico está ou não armazenado dentro da lista
+
+# elem in my_list
+# elem not in my_list
+
+my_list = [0, 3, 12, 8, 2]
+
+print(5 in my_list)
+print(5 not in my_list)
+print(12 in my_list)
+
